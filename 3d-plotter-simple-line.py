cfe7b95e86b5ba099aa99a018532e2a885c5c33f
Niklas <lord.voldemort1492@gmail.com>
2020-04-04 18:46:21 +0200

    Added the 3d plotting, now in functions, program stopped because of missing fun

--- 3d-plotter-simple-line.py
+++ 3d-plotter-simple-line.py
@@ -3,12 +3,42 @@ import matplotlib.pyplot as plt
 import datetime
 import os
 
+def plot_surface_cube(x,y,z,a=0.5):
+    X=(0,x)
+    Y=(0,y)
+    X, Y = np.meshgrid(X, Y)
+    one = np.zeros(4).reshape(2, 2)
+    one = one+z
+    ax.plot_surface(X,Y,one, alpha=0.3)
+    ax.plot_wireframe(X,Y,-one, alpha=0.5)
+    ax.plot_wireframe(X,-one,Y, alpha=0.5)
+
+def plot_line(x,y,z,a=0.5):
+    # Data for a three-dimensional line
+    zline = np.linspace(0, 2, 1000)
+    xline = [0] * 1000
+    yline = [0] * 1000
+    #ax.plot(xline, yline, zline, 'b')
+
+def animation(max_angle_z,max_angle_y,path,folder,state=True):
+    # animation for pictures
+    for angle2 in max_angle_z:
+        last_angle = 0
+        for angle in range(0, max_angle_y):
+            if last_angle != angle2 and state:
+                file = 'pic' + str(angle2) + str(angle)
+                plt.savefig(path + folder + '/' + file)
+            ax.view_init(angle2, angle)
+            plt.draw()
+            plt.pause(.01)
+            last_angle = angle2
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
 # configs for animation
 max_angle_y = 180
-max_angle_z = [20, 40, 75]
+max_angle_z = [90]
 
 # path of pictures
 path = '/home/niklas/Desktop/School-Stuff/Data/3d-plotter-simple-data/'
@@ -17,20 +47,10 @@ folder = str(datetime.datetime.now())
 # create folder for today
 os.mkdir(path + folder)
 
-# Data for a three-dimensional line
-zline = np.linspace(0, 15, 1000)
-xline = [5] * 1000
-yline = [4] * 1000
-ax.plot(xline, yline, zline, 'b')
+# setting the labels
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 
-# animation for pictures
-for angle2 in max_angle_z:
-    last_angle = 0
-    for angle in range(0, max_angle_y):
-        if last_angle != angle2:
-            file = 'pic' + str(angle2) + str(angle)
-            plt.savefig(path + folder + '/' + file)
-        ax.view_init(angle2, angle)
-        plt.draw()
-        plt.pause(.01)
-        last_angle = angle2
+plot_surface_cube(2,3,4)
+plt.show()
