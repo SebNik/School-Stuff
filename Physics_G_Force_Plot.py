c18a58c465b80545bcafde7167df74f058f4add8
Niklas <lord.voldemort1492@gmail.com>
2020-09-22 17:24:53 +0200

    added new files for g force plot

---
+++ Physics_G_Force_Plot.py
@@ -0,0 +1,37 @@
+# this file is solving the problem of physics
+import math
+import random
+from scipy.constants import pi, G
+import matplotlib.pyplot as plt
+
+
+def force_earth_globe(n, dr, m_s, rho, dphi, k_):
+    # adding the sums of the single forces
+    # setting force sum adn mass sum
+    sum_f = 0
+    sum_m = 0
+    # iterating through steps
+    for i in range(n_steps):
+        phi = dphi * i
+        a = math.cos(phi) - k_
+        r_r = math.sin(phi)
+        r = math.sqrt(a ** 2 + r_r ** 2)
+        V_r = pi * 2 * r_r * dphi * dr
+        m_r = V_r * rho
+        F = G * m_s * m_r * a * (r ** 3)
+        sum_f += F
+        sum_m += m_r
+    # returning values
+    return sum_f, sum_m
+
+
+if __name__ == "__main__":
+    n_steps = 100
+    DR = 0.1
+    m_stein = 1
+    # Rho = 5510
+    Rho = 3000
+    DPhi = pi / n_steps
+    k = 1.1
+    # collecting samples
+    print(force_earth_globe(n=n_steps, dr=DR, m_s=m_stein, rho=Rho, dphi=DPhi, k_=k))
